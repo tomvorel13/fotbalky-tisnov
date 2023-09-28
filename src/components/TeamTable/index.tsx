20adac597663136619f9b3228dda047f6959e3fd
Tom Vorel <tomvorel13@gmail.com>
2023-09-28 14:42:25 +0200

    Table clean up

--- src/components/TeamTable/index.tsx
+++ src/components/TeamTable/index.tsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import { Table, ScrollArea } from '@mantine/core'
-import cx from 'clsx'
+import { Table } from '@mantine/core'
 import classes from './TeamTable.module.css'
 import { Team } from '../../types'
 
 export function TeamTable({ team }: { team: Team }) {
-	const [scrolled, setScrolled] = useState(false)
-
 	const rows = team.players.map((player) => (
 		<Table.Tr key={player.name}>
 			<Table.Td>{player.name}</Table.Td>
@@ -14,20 +10,13 @@ export function TeamTable({ team }: { team: Team }) {
 	))
 
 	return (
-		<ScrollArea
-			h={300}
-			onScrollPositionChange={({ y }) => setScrolled(y !== 0)}
-		>
-			<Table miw={700}>
-				<Table.Thead
-					className={cx(classes.header, { [classes.scrolled]: scrolled })}
-				>
-					<Table.Tr>
-						<Table.Th>Name</Table.Th>
-					</Table.Tr>
-				</Table.Thead>
-				<Table.Tbody>{rows}</Table.Tbody>
-			</Table>
-		</ScrollArea>
+		<Table className={classes.table}>
+			<Table.Thead className={classes.header}>
+				<Table.Tr>
+					<Table.Th>Name</Table.Th>
+				</Table.Tr>
+			</Table.Thead>
+			<Table.Tbody>{rows}</Table.Tbody>
+		</Table>
 	)
 }
